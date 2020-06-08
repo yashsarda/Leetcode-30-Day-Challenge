c32b441fdf7641e2475c183c22270bf75664da9c
yashsarda <yash1998sarda@gmail.com>
2020-06-08 15:42:16 +0530

    Day 8

---
+++ June/Day-8 Power of Two.cpp
@@ -0,0 +1,12 @@
+/*
+Given an integer, write a function to determine if it is a power of two.
+ */
+ class Solution {
+ public:
+     bool isPowerOfTwo(int n) {
+         if(n <= 0)
+             return false;
+
+         return ((n & (n - 1)) == 0);
+     }
+ };
